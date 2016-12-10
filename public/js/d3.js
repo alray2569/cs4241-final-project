af10f07253a59b161e1706440d16a72a6802686f
Andrew Ray <alr2569@gmail.com>
2016-12-10 18:24:46 -0500

    Errata - Add copy notice - Add comments to d3.js - Fix small issue wherein << and >> would not disappear in smheader due to off-by-three mistake

--- public/js/d3.js
+++ public/js/d3.js
@@ -1,5 +1,6 @@
 /* global d3: false */
 
+// converts an object into an array of key-value pairs.
 function toKV(obj) {
 	var kva = [], x, key;
 	
@@ -11,11 +12,14 @@ function toKV(obj) {
 	return kva;
 }
 
+// normalizes values in the range [inmin - inmax] to values in the
+// range [outmin - outmax].
 function scale(value, inmin, inmax, outmin, outmax) {
 	return (value - inmin) / (inmax - inmin) * (outmax - outmin) + outmin;
 }
 
 function d3Do () {
+	// Empty the elements first, especially important on resize.
 	document.getElementById("graphClassSize").innerHTML = "";
 	document.getElementById("graphFaculSize").innerHTML = "";
 	document.getElementById("graphCOS").innerHTML = '';
@@ -25,6 +29,13 @@ function d3Do () {
 		mode = window.getComputedStyle(
 			document.getElementById("mode")
 		).zIndex - 1,
+		
+		/*
+			MODE is the type of browser:
+			0: computer, width > 600px (use two-column layout)
+			1: computer, width < 600px (use one-column layout)
+			2: mobile (use one-column layout + sizing overhaul)
+		*/
 		
 		margin = {
 			top: mode === 2 ? 15 : 10, 
@@ -41,24 +52,48 @@ function d3Do () {
 			mode === 2?
 				600:
 				300,
-
+		
+		// Tell the graph to fit in the graphic
 		x = d3.scale.linear().range([0, width]),
 		y = d3.scale.linear().range([height, 0]),
 
+		// tell the graph how to draw the axes
 		xAxis = d3.svg.axis().scale(x).orient("bottom")
+			// use the values of years as major ticks
 			.tickValues(window.years)
 			.tickFormat(function (datum, index) {
 				return mode === 2 || width < 300?
+					// in mobile (mode 2) and narrow graphs
+					// (width < 300px), abbreviate the year 
+					//from, say, 1865, to ’65.
 					'’' + (datum + "").substring(2):
+					// in wider graphs, don't abbreviate
 					datum;
 			}),
 		yAxis = d3.svg.axis().scale(y).orient("left").ticks(5),
 
+		// how to draw the graph line
 		valueline = d3.svg.line()
-					.x(function(d) {return scale(d.key, 1865, 1935, 0, width);})
-					.y(function(d) {return height - scale(d.value, 0, d3.max(data, function(d) {return d.value;}), 0, height);})
+					.x(function(d) {
+						// x axis needs to be scaled
+						return scale(d.key, 1865, 1935, 0, width);
+					})
+					.y(function(d) {
+						// as does y.
+						return height - scale(
+							d.value,
+							0, 
+							// the maximum value in y.
+							d3.max(data, function(d) {
+								return d.value;
+							}),
+							0,
+							height);
+					})
+					// fill in the lines between.
 					.interpolate("linear"),
 
+		// Add an svg element under the #graphClassSize tag
 		gcs = d3.select("#graphClassSize")
 				.append("svg")
 					.attr("width", width + margin.left + margin.right)
@@ -66,6 +101,7 @@ function d3Do () {
 				.append("g")
 					.attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
 		
+		// under the #graphFaculSize
 		gfs = d3.select("#graphFaculSize")
 				.append("svg")
 					.attr("width", width + margin.left + margin.right)
@@ -73,6 +109,7 @@ function d3Do () {
 				.append("g")
 					.attr("transform", "translate(" + margin.left + ", " + margin.top + ")"),
 		
+		// under the #graphCOS
 		cos = d3.select("#graphCOS")
 				.append("svg")
 					.attr("width", width + margin.left + margin.right)
@@ -80,6 +117,7 @@ function d3Do () {
 				.append("g")
 					.attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
 		
+		// and under the #graphTui
 		tui = d3.select("#graphTui")
 				.append("svg")
 					.attr("width", width + margin.left + margin.right)
@@ -87,34 +125,41 @@ function d3Do () {
 				.append("g")
 					.attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
 	
+		// convert all the data sets to key-val pairs
 		data = toKV(window.avgclass),
 		data2= toKV(window.faculty),
 		data3= toKV(window.majors),
 		data4= toKV(window.tuition);
 	
-	// 
-	
+	// CLASS SIZE GRAPH
+	// set the axis ranges
 	x.domain(d3.extent(data, function(d) {return d.key;}));
 	y.domain([0, d3.max(data, function(d) {return d.value;})]);
 	
+	// add the graphline
 	gcs.append("path")
 		.attr("class", "line")
 		.attr("d", valueline(data));
 	
+	// add the x-axis
 	gcs.append("g")
 		.attr("class", "x axis")
 		.attr("transform", "translate(0, " + height + ")")
 		.call(xAxis);
 	
+	// add the y-axis
 	gcs.append("g")
 		.attr("class", "y axis")
 		.call(yAxis);
 	
+	// FACULTY SIZE GRAPH
+	// need to adjust the valueline function to use new max y.
 	valueline = d3.svg.line()
 		.x(function(d) {return scale(d.key, 1865, 1935, 0, width);})
 		.y(function(d) {return height - scale(d.value, 0, d3.max(data2, function(d) {return d.value;}), 0, height);})
 		.interpolate("linear");
 	
+	// The rest here follows as above
 	x.domain(d3.extent(data2, function(d) {return d.key;}));
 	y.domain([0, d3.max(data2, function(d) {return d.value;})]);
 	
@@ -131,6 +176,8 @@ function d3Do () {
 		.attr("class", "y axis")
 		.call(yAxis);
 	
+	
+	// COURSES OF STUDY
 	valueline = d3.svg.line()
 		.x(function(d) {return scale(d.key, 1865, 1935, 0, width);})
 		.y(function(d) {return height - scale(d.value, 0, d3.max(data3, function(d) {return d.value;}), 0, height);})
@@ -152,6 +199,9 @@ function d3Do () {
 		.attr("class", "y axis")
 		.call(yAxis);
 	
+	// TUITION
+	// Note that for this graph, we divide the y values
+	// by 100 for display on the graph.
 	valueline = d3.svg.line()
 		.x(function(d) {return scale(d.key, 1865, 1935, 0, width);})
 		.y(function(d) {return height - scale(d.value / 100, 0, d3.max(data4, function(d) {return d.value / 100;}), 0, height);})
