0954ddc08eeaaa84053d612b343db780becb68c7
Andrew Ray <alr2569@gmail.com>
2016-12-13 16:34:10 -0500

    Changing URL with years

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,8 @@ var app = new angular.module("WPIHistory", []);
 
 app.controller("controller", function ($scope, $http, $sce) {
 	"use strict";
-	
-	$scope.years = window.years;
-	
-	$scope.majors = window.majors;
-	$scope.avgclass = window.avgclass;
-	$scope.faculty = window.faculty;
-	$scope.tuition = window.tuition;
-	
-	$scope.year = $scope.years[0];
-	
+
+	// FUNCTION DEFINITIONS
 	$scope.goto = function (year) {
 		$scope.year = year;
 		$scope.updateMap(year);
@@ -21,17 +13,33 @@ app.controller("controller", function ($scope, $http, $sce) {
 	$scope.updateMap = window.updateMap;
 	$scope.updatePresPic = window.updatePresPic;
 	
+	$scope.getHtml = function (text) {
+		return $sce.trustAsHtml(text);
+	};
+	
+	// VARIABLE DEFINITIONS
+	
+	$scope.years = window.years;
+	
+	$scope.majors = window.majors;
+	$scope.avgclass = window.avgclass;
+	$scope.faculty = window.faculty;
+	$scope.tuition = window.tuition;
+	
+	$scope.year = parseInt(window.location.hash.substr(1), 10);
+	if ($scope.years.indexOf($scope.year) === -1) {
+		$scope.year = $scope.years[0];
+	}
+	
+	// STUFF TO DO ON STARTUP
+	
 	$http.get("text.json")
 		.then(
 			function success (response) {
 				$scope.text = response.data;
+				$scope.goto($scope.year);
 			}, function failure (response) {
 				window.console.log(response);
 			}
 		);
-	
-	
-	$scope.getHtml = function (text) {
-		return $sce.trustAsHtml(text);
-	};
 });
